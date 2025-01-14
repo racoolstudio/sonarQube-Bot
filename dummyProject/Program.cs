908df45d8f15817f883f750046abd36f02a3f1ce
Ridwan Abdulwaheed <75172422+racoolstudio@users.noreply.github.com>
2025-01-14 16:16:13 -0330

    Update Program.cs

--- dummyProject/Program.cs
+++ dummyProject/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 var password33="passworfdfe";
+var username="passworfdfe";
+var password="passworfdfe";
